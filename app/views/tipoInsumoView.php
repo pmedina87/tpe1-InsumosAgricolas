82277097bbe6ac5b91ddea41f10311cb232da52c
Pedro Medina <pedrohugomedina@gmail.com>
2022-10-03 08:27:50 -0300

    Comienzo a usar Smarty

--- app/views/tipoInsumoView.php
+++ app/views/tipoInsumoView.php
@@ -1,38 +1,27 @@
 <?php
 
+require_once './libs/smarty-4.2.1/libs/Smarty.class.php';
 
 class tipoInsumoView{
+
+    private $smarty;
+
+    /**
+     * Constructor que instancia Smarty.
+     */
+    public function __construct() {
+        $this->smarty = new Smarty();
+    }
 
     /**
      * Consulta para mostrar todos los tipos de insumos
      */
     function renderAllTiposInsumos($tiposInsumos){
-        echo "<h1  class='m-3'>Lista de Tipos de Insumos</h1>";
-        // tabla de tipos de insumos
-        echo "<table class='table'>
-        <thead>
-            <tr>
-                <th>Codigo</th>
-                <th>Tipo de Insumo</th>
-                <th>Acciones</th>
-            </tr>
-        <thead>
-        <tbody>";
-
-        foreach($tiposInsumos as $tipoInsumo) {
-            echo "
-                <tr>
-                    <td>$tipoInsumo->id_tipo_insumo</td>
-                    <td>$tipoInsumo->tipo_insumo</td>
-                    <td>
-                    <a href='Edit_Tipo_Insumo/$tipoInsumo->id_tipo_insumo' type='button' class='btn btn-primary ml-auto'>Editar</a> 
-                    <a href='Delete_Tipo_Insumo/$tipoInsumo->id_tipo_insumo' type='button' class='btn btn-danger ml-auto'>Eliminar</a>
-                    </td>
-                </tr>
-            ";
-            }
-        echo " </tbody>   
-        </table>";
+        $cant_elementos = count($tiposInsumos);
+        $this->smarty->assign('titulo', "Listado de Tipos de Insumos");
+        $this->smarty->assign('tiposInsumos', $tiposInsumos);
+        $this->smarty->assign('cantidad', $cant_elementos);
+        $this->smarty->display('tiposInsumosList.tpl');
     }
     
     /**
@@ -55,52 +44,43 @@ class tipoInsumoView{
      * Muestra los datos de un insumo determinado, para poder editarlo
      */
     function renderUpdateTipoInsumoById($tipoInsumo) {
+        $titulo = "Actualizar Tipo de Insumo";
         $id = $tipoInsumo->id_tipo_insumo;
+        $action = "Update_Tipo_Insumo";
         $tipoInsumo = $tipoInsumo->tipo_insumo;
-        echo '<h2 class="m-3">Actualizar Tipo Insumo</h2>
-
-        <form action="Update_Tipo_Insumo" method="POST">
-            <div class="m-3">
-                <input type="number" id="id" name="id" value="'.$id.'" hidden>
-            </div>
-            <div class="m-3">
-                <label for="tipo_insumo">Tipo de Insumo:</label>
-                <input type="text" id="tipo_insumo" name="tipo_insumo" value="'.$tipoInsumo.'">
-            </div>
-            <div class="m-3">  
-                <input type="submit" class="btn btn-success ml-auto" value="Actualizar">
-                <a href="Tipos_Insumos">
-                    <input type="button" class="btn btn-danger ml-auto" value="Cancelar">
-                </a>
-            </div>
-
-        </form>';        
+        $boton = "Actualizar";
+        $this->smarty->assign('titulo', $titulo);
+        $this->smarty->assign('action', $action);
+        $this->smarty->assign('id', $id);
+        $this->smarty->assign('tipoInsumo', $tipoInsumo);
+        $this->smarty->assign('boton', $boton);
+        $this->smarty->display('tipoInsumoAdd_Update.tpl');        
     }
 
     /**
      * Render para agregar un tipo de insumo
      */
     function renderAddTipoInsumo() {
+        $titulo = "Alta Tipo Insumo";
+        $action = "Save_Tipo_Insumo";
+        $boton = "Guardar";
+        $this->smarty->assign('titulo', $titulo);
+        $this->smarty->assign('action', $action);
+        $this->smarty->assign('boton', $boton);
+        $this->smarty->display('tipoInsumoAdd_Update.tpl');
+    }
 
-        echo '<h2 class="m-3">Alta Tipo Insumo</h2>
-
-        <form action="Save_Tipo_Insumo" method="POST">
-            <div class="m-3">
-                <input type="text" id="id" name="id" value="" hidden>
-            </div>
-            <div class="m-3">
-                <label for="tipo_insumo">Tipo de Insumo:</label>
-                <input type="text" id="tipo_insumo" name="tipo_insumo" value="">
-            </div>
-            <div class="m-3">  
-                <input type="submit" class="btn btn-success ml-auto" value="Agregar">
-                <a href="Tipos_Insumos">
-                    <input type="button" class="btn btn-danger ml-auto" value="Cancelar">
-                </a>
-            </div>
-
-        </form>';
-        // include 'templates/form_alta_tipo_insumo.php';
+    /**
+     * Funcion que renderiza un detalle de un tipo de insumo especifico
+     */
+    function renderDetailTipoInsumo($tipoInsumo){
+        $titulo = "Detalle de Tipo de Insumo";
+        $nombre = $tipoInsumo->tipo_insumo;
+        $boton = "Cerrar";
+        $this->smarty->assign('titulo', $titulo);
+        $this->smarty->assign('tipoInsumo', $nombre);
+        $this->smarty->assign('boton', $boton);
+        $this->smarty->display('detailTipoInsumo.tpl');
     }
 }
      
