411b0d36d071a312ce8969e375a598e3e2fd162f
Pedro Medina <pedrohugomedina@gmail.com>
2022-10-03 08:26:54 -0300

    Comienzo a usar Smarty

--- app/views/insumoView.php
+++ app/views/insumoView.php
@@ -1,41 +1,29 @@
 <?php
 
+require_once './libs/smarty-4.2.1/libs/Smarty.class.php';
 
 class insumoView{
+
+    private $smarty;
+
+    /**
+     * Constructor que instancia Smarty.
+     */
+    public function __construct() {
+        $this->smarty = new Smarty();
+    }
 
     /**
      * Funcion que renderiza una tabla con todos los insumos
      */
-    function renderAllInsumos($insumos){
-        echo "<h1 class='m-3'>Lista de Insumos</h1>";
-        echo "<table class='table'>
-        <thead>
-            <tr>
-                <th>Codigo</th>
-                <th>Insumo</th>
-                <th>Unidad de Medida</th>
-                <th>Tipo de Insumo</th>
-                <th>Acciones</th>
-            </tr>
-        <thead>
-        <tbody>";
-
-        foreach($insumos as $insumo) {
-            echo "
-                <tr>
-                    <td>$insumo[id_insumo]</td>
-                    <td>$insumo[insumo]</td>
-                    <td>$insumo[unidad_medida]</td>
-                    <td>$insumo[tipo_insumo]</td>
-                    <td>
-                    <a href='Edit_Insumo/$insumo[id_insumo]' type='button' class='btn btn-primary ml-auto'>Editar</a> 
-                    <a href='Delete_Insumo/$insumo[id_insumo]' type='button' class='btn btn-danger ml-auto'>Eliminar</a>
-                    </td>
-                </tr>
-            ";
-            }
-        echo " </tbody>   
-        </table>";
+    function renderAllInsumos($insumos, $tiposInsumos){
+        $cant_elementos = count($insumos);
+        $titulo = "Listado de Insumos";
+        $this->smarty->assign('titulo', $titulo);
+        $this->smarty->assign('insumos', $insumos);
+        $this->smarty->assign('tiposInsumos', $tiposInsumos);
+        $this->smarty->assign('cantidad', $cant_elementos);
+        $this->smarty->display('insumosList.tpl');
     }
     
     /**
@@ -57,55 +45,55 @@ class insumoView{
     /**
      * Funcion que renderiza todos los datos de un insumo determinado, para poder editarlo
      */
-    function renderInsumoById($insumo) {
-        // include 'templates/form_alta_insumo.php';
-        // $id = $insumo['id_insumo'];
-        // $insumo = $insumo['insumo'];
-        // $unidad_medida = $insumo['unidad_medida'];
-        // $id_tipo_insumo = $insumo['id_tipo_insumo'];
+    function renderFormUpdateInsumoById($insumo, $tipos_Insumos) {
+        $titulo = "Actualizar Insumo";
+        $action = "Update_Insumo";
         $id = $insumo->id_insumo;
-        $insumo = $insumo->insumo;
+        $insumo_nombre = $insumo->insumo;
         $unidad_medida = $insumo->unidad_medida;
-        $id_tipo_insumo = $insumo->id_tipo_insumo;
-        $tipo_insumo = $insumo->tipo_insumo;
-        var_dump($unidad_medida);
-        var_dump($tipo_insumo);
-        var_dump($id_tipo_insumo);
-        var_dump($id, $insumo, $unidad_medida, $id_tipo_insumo);
+        $id_tipoInsumo = $insumo->id_tipo_insumo;
+        $boton = "Actualizar";
+        $this->smarty->assign('titulo', $titulo);
+        $this->smarty->assign('action', $action);
+        $this->smarty->assign('id', $id);
+        $this->smarty->assign('insumo', $insumo_nombre);
+        $this->smarty->assign('unidad_medida', $unidad_medida);
+        $this->smarty->assign('idTipoInsumo', $id_tipoInsumo);
+        $this->smarty->assign('tiposInsumos', $tipos_Insumos);
+        $this->smarty->assign('boton', $boton);
+        $this->smarty->display('insumoAdd_Update.tpl');
     }
 
     /**
      * Funcion que renderiza un formulario para agregar un nuevo insumo
      */
     function renderAddInsumo($tipos_insumos) {
-        echo '<h2 class="m-3">Alta de Insumo</h2>
+        $titulo = "Alta de Insumo";
+        $action = "Save_Insumo";
+        $boton = "Guardar";
+        $this->smarty->assign('titulo', $titulo);
+        $this->smarty->assign('action', $action);
+        $this->smarty->assign('tiposInsumos', $tipos_insumos);
+        $this->smarty->assign('boton', $boton);
+        $this->smarty->display('insumoAdd_Update.tpl');
+    }
 
-        <form action="Save_Insumo" method="POST">
-            <div class="mb-3">
-                <label for="insumo">Insumo:</label>
-                <input type="text" id="insumo" name="insumo" value="">
-            </div>
-            <div class="mb-3">
-                <label for="unidad_medida">Unidad de Medida:</label>
-                <input type="text" id="unidad_medida" name="unidad_medida" value="">
-            </div>
-            <div class="mb-3">
-                <label for="tipo_insumo">Tipo de Insumo:</label>
-                <input type="hidden" name="tipo_insumo">
-                    <select id = "tipo_insumo" name = "tipo_insumo">
-                        <option value="0">Seleccione Tipo de Insumo:</option>';
-                            foreach ($tipos_insumos as $tipo) {
-                                echo '<option value="'.$tipo->id_tipo_insumo.'">'.$tipo->tipo_insumo.'</option>';
-                            }
-                    echo '</select>
-            </div>
-            <div class="m-3">
-                <input type="submit" class="btn btn-success ml-auto" value="Agregar">
-                <a href="Insumos">
-                    <input type="button" class="btn btn-danger ml-auto" value="Cancelar">
-                </a>
-            </div>
-        </form>'; 
+    /**
+     * Funcion que renderiza un detalle de un insumo especifico
+     */
+    function renderDetailInsumo($insumo, $tiposInsumos) {
+        $titulo = "Detalle de Insumo";
+        $nombre = $insumo->insumo;
+        $unidad_medida = $insumo->unidad_medida;
+        $id_tipo_insumo = $insumo->id_tipo_insumo;
+        $boton = "Cerrar";
+        $this->smarty->assign('titulo', $titulo);
+        $this->smarty->assign('insumo', $nombre);
+        $this->smarty->assign('unidadMedida', $unidad_medida);
+        $this->smarty->assign('idTipoInsumo', $id_tipo_insumo);
+        $this->smarty->assign('tiposInsumos', $tiposInsumos);
+        $this->smarty->assign('boton', $boton);
+        $this->smarty->display('detailInsumo.tpl');
     }
 }
      
