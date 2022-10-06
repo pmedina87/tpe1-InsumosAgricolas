17e9e385da595a77928597aeb9bd4bcccb39c910
Pedro Medina <pedrohugomedina@gmail.com>
2022-10-06 11:16:14 -0300

    Agrego archivo messageView para renderizar mensajes al usuario

---
+++ app/views/messageView.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once './libs/smarty-4.2.1/libs/Smarty.class.php';
+
+class messageView{
+
+    private $smarty;
+
+    /**
+     * Constructor que instancia Smarty.
+     */
+    public function __construct() {
+        $this->smarty = new Smarty();
+    }
+   
+    /**
+     * Muestra pantalla de error
+     */
+    function message($titulo, $msg, $redireccion) {
+        $boton = "Aceptar";
+        $this->smarty->assign('titulo', $titulo);
+        $this->smarty->assign('msg', $msg);
+        $this->smarty->assign('redireccion', $redireccion);
+        $this->smarty->assign('boton', $boton);
+        $this->smarty->display('msgError.tpl');
+    }
+}
