cfc23ebfc8af8bca5f3c70b2bbc584f1bc33cb98
Pedro Medina <pedrohugomedina@gmail.com>
2022-10-06 11:24:46 -0300

    Agrego archivo usuarioView.php

---
+++ app/views/usuarioView.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once './libs/smarty-4.2.1/libs/Smarty.class.php';
+
+class usuarioView{
+
+    private $smarty;
+
+    /**
+     * Constructor que instancia Smarty.
+     */
+    public function __construct() {
+        $this->smarty = new Smarty();
+    }
+
+    public function renderFormLogin() {
+        $titulo = "Login";
+        $this->smarty->assign('titulo', $titulo);
+        $this->smarty->display('loginForm.tpl');
+    }
+
+    public function renderUsuariosList($usuarios) {
+        $titulo = "Listado de Usuarios";
+        $cantidad = count($usuarios);
+        $this->smarty->assign('titulo', $titulo);
+        $this->smarty->assign('cantidad', $cantidad);
+        $this->smarty->assign('usuarios', $usuarios);
+        $this->smarty->display('usuariosList.tpl');
+    }
+
+    public function renderFormAddUsuario() {
+        $titulo = "Agregar un Usuario";
+        $action = "Save_Usuario";
+        $boton = "Guardar";
+        $this->smarty->assign('titulo', $titulo);
+        $this->smarty->assign('action', $action);
+        $this->smarty->assign('boton', $boton);
+        $this->smarty->display('usuarioAdd_Update.tpl');
+    }
+
+
+}
