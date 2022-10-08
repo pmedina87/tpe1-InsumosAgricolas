93096e568d5d068501433da69595b7dff15a8860
Pedro Medina <pedrohugomedina@gmail.com>
2022-10-07 21:45:32 -0300

    Incluyo a AuthHelper para verificar en cada url si el usuario esta logeado

--- app/controllers/usuarioController.php
+++ app/controllers/usuarioController.php
@@ -1,37 +1,33 @@
 <?php
 
-
 require_once 'app/models/usuarioModel.php';
 require_once 'app/views/usuarioView.php';
 require_once 'app/views/messageView.php';
+require_once 'app/helpers/authHelper.php';
 
 
-class usuarioController{
+class UsuarioController{
 
     private $usuarioModel;
     private $usuarioView;
     private $messageView;
+    private $authHelper;
 
     /**
      * Constructor de la clase usuarioController.
      */
     public function __construct() {
-        $this->usuarioModel = new usuarioModel();
-        $this->usuarioView = new usuarioView();
-        $this->messageView = new messageView();
-    }
-    
-    /**
-     * Funcion que muestra el formulario de login.
-     */
-    function showFormLogin(){
-        $this->usuarioView->renderFormLogin();
+        $this->usuarioModel = new UsuarioModel();
+        $this->usuarioView = new UsuarioView();
+        $this->messageView = new MessageView();
+        $this->authHelper = new AuthHelper();            
     }
 
     /**
      * Funcion que muestra todos los usuarios.
      */
     function showAllUsuario(){
+        $this->authHelper->checkSessionActive();
         $usuarios = $this->usuarioModel->getAll();
         $this->usuarioView->renderUsuariosList($usuarios);
     }
@@ -40,6 +36,7 @@ class usuarioController{
      * Funcion que muestra el formulario para agregar un nuevo usuario.
      */
     function showFormAddUsuario(){
+        $this->authHelper->checkLoggedIn();
         $this->usuarioView->renderFormAddUsuario();
     }
 
@@ -47,6 +44,7 @@ class usuarioController{
      * Funcion que guarda los datos de un nuevo usuario.
      */
     function saveNewUsuario(){
+        $this->authHelper->checkLoggedIn();
         $nombre = $_POST['nombre_usuario'];
         $apellido = $_POST['apellido_usuario'];
         $email = $_POST['email_usuario'];
@@ -61,7 +59,7 @@ class usuarioController{
                     $msg = "El usuario ya se encuentra registrado";
                     $redireccion = "Usuarios";
                     $this->messageView->message($titulo, $msg, $redireccion);
-                    return 0;
+                    break;
                 }
                 else {
                     $i++;
@@ -86,6 +84,7 @@ class usuarioController{
      * Funcion que elimina a un usuario.
      */
     function deleteUsuarioById($id){
+        $this->authHelper->checkLoggedIn();
         $this->usuarioModel->delete($id);
         header("Location: " . BASE_URL . "Usuarios");
     }
@@ -94,6 +93,7 @@ class usuarioController{
      * Funcion que muestra el formulario para actualizar los datos de un usuario.
      */
     function showFormUpdateUsuarioById($id){
+        $this->authHelper->checkLoggedIn();
         $usuario = $this->usuarioModel->getById($id);
         $this->usuarioView->showFormUpdateUsuarioById($usuario);
     }
@@ -102,6 +102,7 @@ class usuarioController{
      * Funcion que guarda los datos actualizados de un usuario.
      */
     function saveUpdateUsuario(){
+        $this->authHelper->checkLoggedIn();
         $id_usuario = $_POST['id'];
         $nombre = $_POST['nombre_usuario'];
         $apellido = $_POST['apellido_usuario'];
@@ -109,20 +110,4 @@ class usuarioController{
         $this->usuarioModel->update($id_usuario, $nombre, $apellido, $email);
         header("Location: " . BASE_URL . "Usuarios");
     }
-
-    /**
-     * Funcion que corrobora que los datos ingresados en Login, sean validos con un usuario logeado.
-     */
-    function systemLogin(){
-        $usuario = $this->usuarioModel->getByUsername($_POST['user']);
-        $hash = $usuario->contrasenia;
-        $pass = $_POST['password'];
-        if (password_verify($pass, $hash)){
-            echo "Acceso aceptado";
-        }
-        else{
-            echo "Acceso denegado";
-        }
-    }
-
 }
