e65b7b7e54582389b68dbdec5d2ce329b35f8895
Pedro Medina <pedrohugomedina@gmail.com>
2022-10-07 21:44:31 -0300

    Incluyo a AuthHelper para verificar en cada url si el usuario esta logeado

--- app/controllers/insumoController.php
+++ app/controllers/insumoController.php
@@ -4,27 +4,31 @@ require_once 'app/models/insumoModel.php';
 require_once 'app/models/tipoInsumoModel.php';
 require_once 'app/views/insumoView.php';
 require_once 'app/views/messageView.php';
+require_once 'app/helpers/authHelper.php';
 
-class insumoController{
+class InsumoController{
     private $insumoModel;
     private $tipoInsumoModel;
     private $insumoView;
     private $messageView;
+    private $authHelper;
 
     /**
      * Constructor de la clase insumoController 
     */
     public function __construct() {
-        $this->insumoModel = new insumoModel();
-        $this->tipoInsumoModel = new tipoInsumoModel();
-        $this->insumoView = new insumoView();
-        $this->messageView = new messageView();
+        $this->insumoModel = new InsumoModel();
+        $this->tipoInsumoModel = new TipoInsumoModel();
+        $this->insumoView = new InsumoView();
+        $this->messageView = new MessageView();
+        $this->authHelper = new AuthHelper();
     }
 
     /**
      * Funcion que muestra todos los insumos
      */
     function showAllInsumos(){
+        $this->authHelper->checkSessionActive();
         $insumos = $this->insumoModel->getAll();
         $tiposInsumos = $this->tipoInsumoModel->getAll();
         $this->insumoView->renderAllInsumos($insumos, $tiposInsumos);
@@ -34,15 +38,16 @@ class insumoController{
      * Funcion que muestra un insumo especifico
      */
     function showInsumoById($id){
+        $this->authHelper->checkSessionActive();
         $insumo = $this->insumoModel->getById($id);
         return $insumo;
     }
-
 
     /**
      * Funcion que muestra un insumo especifico para actualizarlo
      */
     function showFormUpdateInsumoById($id){
+        $this->authHelper->checkLoggedIn();
         $insumo = $this->showInsumoById($id);
         $tipoInsumo = $this->tipoInsumoModel->getAll();
         $this->insumoView->renderFormUpdateInsumoById($insumo, $tipoInsumo);
@@ -52,6 +57,7 @@ class insumoController{
      * Funcion que elimina un insumo
      */
     function deleteInsumoById($id){
+        $this->authHelper->checkLoggedIn();
         $this->insumoModel->delete($id);
         header("Location: " . BASE_URL . 'Insumos'); 
     }
@@ -60,6 +66,7 @@ class insumoController{
      * Funcion que muestra el formulario para agregar un nuevo insumo
      */
     function showFormAddInsumo(){
+        $this->authHelper->checkLoggedIn();
         $tipos_insumos = $this->tipoInsumoModel->getAll();
         $this->insumoView->renderAddInsumo($tipos_insumos);
     }
@@ -68,6 +75,7 @@ class insumoController{
      * Funcion que guarda un nuevo insumo
      */
     function saveNewInsumo(){
+        $this->authHelper->checkLoggedIn();
         $insumo = $_POST['insumo'];
         $unidad_medida = $_POST['unidad_medida'];
         $id_tipo_insumo = intval($_POST['tipo_insumo']);
@@ -87,6 +95,7 @@ class insumoController{
      * Funcion que guarda la actualizacion de tipo de insumo
      */
     function saveUpdateInsumo(){
+        $this->authHelper->checkLoggedIn();
         $id = $_POST['id'];
         $insumo = $_POST['insumo'];
         $unidad_medida = $_POST['unidad_medida'];
@@ -107,6 +116,7 @@ class insumoController{
      * Funcion que muestra el detalle de un insumo especifico
      */
     function showInsumo($id){
+        $this->authHelper->checkSessionActive();
         $insumo = $this->showInsumoById($id);
         $tiposInsumos = $this->tipoInsumoModel->getAll();
         $this->insumoView->renderDetailInsumo($insumo, $tiposInsumos);        
@@ -116,6 +126,7 @@ class insumoController{
      * Funcion que filtra insumo segun el tipo
      */
     function showFilterByTipoInsumo() {
+        $this->authHelper->checkSessionActive();
         $id_tipoInsumo = $_POST['tipo_insumo'];
         if ($id_tipoInsumo != 0) {
             $insumos = $this->insumoModel->getByIdTipoInsumo($id_tipoInsumo);
